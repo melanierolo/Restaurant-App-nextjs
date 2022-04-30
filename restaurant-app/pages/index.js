9e2ce894f6a8aa131d3feab489f7c276bcd56c4c
Melanie Rodas <mrodasl@uni.pe>
2022-04-30 16:08:40 -0500

    frontend - we add in reactstrap components

--- restaurant-app/pages/index.js
+++ restaurant-app/pages/index.js
@@ -1,9 +1,17 @@
-import Head from "next/head";
+import { Button, Alert } from "reactstrap";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
+      <div>
+        <div>
+          <Alert color="primary">
+            Hello Project is strapi-next with Bootstrap
+          </Alert>
+          &nbsp; <Button color="primary">Hello from nextjs</Button>
+        </div>
+      </div>
       <h1>Hello world 🙂!! </h1>
     </>
   );

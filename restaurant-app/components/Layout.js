9a7747e557368ea81410153fe220d799f40d1a1f
Melanie Rodas <mrodasl@uni.pe>
2022-04-30 16:55:42 -0500

    frontend - added navbar without logic

---
+++ restaurant-app/components/Layout.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+import { Container, Nav, NavItem } from "reactstrap";
+
+const Layout = (props) => {
+  const title = "Welcome to Nextjs";
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <header>
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            h5 {
+              color: white;
+              padding-top: 11px;
+            }
+          `}
+        </style>
+        <Nav className="navbar navbar-dark bg-dark">
+          <NavItem>
+            <Link href="/">
+              <a className="navbar-brand">Home</a>
+            </Link>
+          </NavItem>
+          <NavItem className="ml-auto">
+            <Link href="/login">
+              <a className="nav-link">Sign In</a>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href="/register">
+              <a className="nav-link"> Sign Up</a>
+            </Link>
+          </NavItem>
+        </Nav>
+      </header>
+      <Container>{props.children}</Container>
+    </div>
+  );
+};
+
+export default Layout;

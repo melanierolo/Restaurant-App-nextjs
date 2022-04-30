ccfbb378e2c0fb16766645affdd7f796ccd48973
Melanie Rodas <mrodasl@uni.pe>
2022-04-30 16:05:15 -0500

    frontend- allowing us to set and manage global styles and shared components in one place of the application

--- restaurant-app/pages/_app.js
+++ restaurant-app/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

f591c2aec526c356276e25e3aa40c8681f0ee3ec
Melanie Rodas <mrodasl@uni.pe>
2022-05-19 12:54:18 -0500

    Adding .env to .gitignore

--- restaurant-app/pages/checkout.js
+++ restaurant-app/pages/checkout.js
@@ -16,7 +16,7 @@ function Checkout() {
 
   // load stripe to inject into elements components
   const stripePromise = loadStripe(
-    "pk_test_51HaLhVGgpfLkdZwmHVQcCOdUzwLWqV7umg9EbicemJqLOcLBPDrPtszruyxf4UzqH0lKwaNj5se3tHldNx92nPjI00Zoi8VgBN"
+    "pk_test_51L0XWTDX7310SgPaeouNJmbyItf1fzre5oSdM6IykDOZzyaO7mfo8msPDus8OD4a2zTPrjRsucvKVycfMeV0nIIJ00t6aq6krD"
   );
 
   return (
